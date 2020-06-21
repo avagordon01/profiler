8ac36913a8a587e727cf7454ffd66c91eda053e1
Ava Gordon <avava01@gmail.com>
2020-06-21 16:27:50 +0100

    add timestamps between probes

--- test_probe.py
+++ test_probe.py
@@ -4,6 +4,7 @@ import bcc
 def run(binary, offset):
     bpf_text = """
     BPF_ARRAY(count, u64, 1);
+    BPF_ARRAY(prev_time, u64, 1);
     int trace(struct pt_regs *ctx) {
         int zero = 0;
         u64* c = count.lookup(&zero);
@@ -11,6 +12,19 @@ def run(binary, offset):
             bpf_trace_printk("step %d\\n", *c);
         }
         count.increment(zero);
+
+        u64* pt = prev_time.lookup(&zero);
+        u64 t = 0;
+        if (pt) {
+            u64 t = *pt;
+        }
+        u64 current_time = bpf_ktime_get_ns();
+        prev_time.update(&zero, &current_time);
+        if (pt) {
+            u64 delta_time = current_time - t;
+            bpf_trace_printk("time delta %lluns\\n", delta_time);
+        }
+
         return 0;
     }
     """
