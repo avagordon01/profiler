5604d7b2b69839a92c0e51980ea70bcd25fb5ee3
Ava Gordon <avava01@gmail.com>
2020-06-21 16:45:34 +0100

    independent state per thread

--- test_probe.py
+++ test_probe.py
@@ -3,26 +3,26 @@ import bcc
 
 def run(binary, offset):
     bpf_text = """
-    BPF_ARRAY(count, u64, 1);
-    BPF_ARRAY(prev_time, u64, 1);
+    BPF_HASH(count, u32, u64);
+    BPF_HASH(prev_time, u32, u64);
     int trace(struct pt_regs *ctx) {
-        int zero = 0;
-        u64* c = count.lookup(&zero);
+        u32 tid = bpf_get_current_pid_tgid();
+
+        u64* c = count.lookup(&tid);
         if (c) {
             bpf_trace_printk("step %d\\n", *c);
         }
-        count.increment(zero);
+        count.increment(tid);
 
-        u64* pt = prev_time.lookup(&zero);
-        u64 t = 0;
+        u64* pt = prev_time.lookup(&tid);
+        u64 current_time = bpf_ktime_get_ns();
         if (pt) {
             u64 t = *pt;
-        }
-        u64 current_time = bpf_ktime_get_ns();
-        prev_time.update(&zero, &current_time);
-        if (pt) {
+            prev_time.update(&tid, &current_time);
             u64 delta_time = current_time - t;
             bpf_trace_printk("time delta %lluns\\n", delta_time);
+        } else {
+            prev_time.insert(&tid, &current_time);
         }
 
         return 0;
