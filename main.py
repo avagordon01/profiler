38104b603d91772e7c1212634d6bc645a9fe0e44
Ava Gordon <avava01@gmail.com>
2020-06-30 15:28:40 +0100

    remove hackneyed profile/funcslower combo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import profile
-import funcslower
 import dwarf
 import test_probe
 
@@ -13,19 +11,8 @@ binary = sys.argv[1]
 filename = sys.argv[2].encode()
 line = int(sys.argv[3])
 
+print('getting address from line number')
 _, address = dwarf.location_to_abs_address(dwarf.load_dwarf(binary), filename, line)
 print('address', hex(address))
+print('attaching probe')
 test_probe.run(binary, address)
-
-exit(0)
-
-profile.user_stacks_only = False
-profile.kernel_stacks_only = False
-bpf_text = profile.preprocess()
-funcslower.get_user_stack = True
-funcslower.get_kernel_stack = True
-bpf_text += '\n' + funcslower.preprocess(["c:open"], 3, None)
-b = profile.compile(bpf_text)
-profile.attach(b)
-profile.wait()
-profile.report(b)
